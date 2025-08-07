cd12f1992bf9b85ddf2c80e41ca3afde582c7061
Mohammad Saikat Islam <saikatislam709@gmail.com>
2025-08-07 11:14:30 +0600

    lab work 6

---
+++ Student Management/src/Student.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+
+public class Student {
+    private int studentId;
+    private String studentName;
+    private String major;
+
+    public Student(int studentId, String studentName, String major) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.major = major;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentName='" + studentName + '\'' +
+                ", major='" + major + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId == student.studentId;
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(studentId);
+    }
+
+}
---
+++ Student Management/src/StudentManagementSystem.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class StudentManagementSystem {
+
+    public static void main(String[] args) {
+        // task 1
+        List<Student> students = new ArrayList<>();
+        add5Students(students);
+        for(Student student : students) {
+            System.out.println(student);
+        }
+
+        students.add(2, new Student(678901, "Eigen", "Biology"));
+        students.remove(1);
+        System.out.println();
+
+        // task 2
+        Set<Student> oopCourse = new HashSet<>();
+        oopCourse.add(students.get(0));
+        oopCourse.add(students.get(1));
+        oopCourse.add(students.get(2));
+        oopCourse.add(students.get(0));
+        System.out.println("Number of students in OOP Course: " + oopCourse.size());
+        if ( oopCourse.contains(students.get(1)) ) {
+            System.out.println("INFO: " + students.get(1) + " is already in OOP Course.");
+        }
+
+        for (Student std: oopCourse) {
+            System.out.println(std);
+        }
+        System.out.println();
+
+
+        // task 3
+        Map<Integer, Student> studentMap = new HashMap<>();
+        for(Student student : students) {
+            studentMap.put(student.getStudentId(), student);
+        }
+        Student std = studentMap.get(678901);
+        System.out.println("From map: " + std);
+    }
+
+    public static void add5Students(List<Student> students) {
+        students.add(new Student(123456, "John Smith", "Computer Science"));
+        students.add(new Student(234567, "Emily Johnson", "Mathematics"));
+        students.add(new Student(345678, "Michael Brown", "Physics"));
+        students.add(new Student(456789, "Sarah Davis", "Biology"));
+        students.add(new Student(567890, "David Wilson", "Chemistry"));
+    }
+}
